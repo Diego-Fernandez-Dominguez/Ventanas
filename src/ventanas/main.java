bd07cac37ca1cf6ad07be40bc951c1ef2730e7dd
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-03-05 20:28:51 +0100

    esto es un caos

---
+++ src/ventanas/main.java
@@ -0,0 +1,11 @@
+package ventanas;
+
+public class main {
+	public main() {
+
+		System.out.println("Me tendria que haber metido en jardineria jaja no es broma");
+		/* que narices he hecho con mi vida */
+
+	}
+
+}
